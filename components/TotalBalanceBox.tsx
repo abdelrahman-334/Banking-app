a6bb7b8ba3202b1153c5a064f3e8d6544e2c7e61
abdel rahman Hassan <st20182841@gmail.com>
2024-06-22 16:50:59 +0300

    added bank accounts number box

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 interface BalanceBoxProps{
     accounts:Array<string>,
-    totalBanks:Number,
-    totalCurrentBalance:Number
+    totalBanks:number,
+    totalCurrentBalance:number
 }
 const TotalBalanceBox = ({accounts=[],totalBanks,totalCurrentBalance}:BalanceBoxProps) => {
   return (
     <section className='total-balance'>
       <div className='total-balance-chart'>
-
+        {/*Chart*/ }
+      </div>
+      <div className='flex flex-col gap-6'>
+        <h2 className='header-2'>
+            {totalBanks} Bank Accounts
+        </h2>
       </div>
     </section>
   )
